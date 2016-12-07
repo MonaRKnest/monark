a2faa8cca83b8ed4f0560405e7a179f6b3cb5721
Jeet <jeet.chanchawat@gmail.com>
2016-12-07 13:19:49 +0530

    Adding own get post methods

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,11 +3,44 @@ import Vapor
 let drop = Droplet()
 
 drop.get { req in
+    let lang = req.headers["Accept-Language"]?.string ?? "en"
     return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+        "message": Node.string(drop.localization[lang, "welcome", "title"])
+        ])
+}
+
+drop.get("version") { request in
+    return try JSON(node: [
+        "version": "1.0"
+        ])
+}
+
+drop.post("json") { request in
+    debugPrint(request.json!)
+    guard let name = request.json?["name"]?.string else {
+        throw Abort.badRequest
+    }
+    
+    return "Hello, \(name)!"
+}
+//
+//drop.post("json") { request in
+//    debugPrint(request.json)
+//    guard let name = request.json?["name"]?.string else {
+//        throw Abort.badRequest
+//    }
+//
+//    return "Hello, \(name)!"
+//}
+
+drop.get("version") { request in
+    return try JSON(node: [
+        "version": "1.0"
+        ])
 }
 
 drop.resource("posts", PostController())
 
+
 drop.run()
+
